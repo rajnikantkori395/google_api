bc6d3f297b4899c89f141300762401a450b649f2
rajnikantkori395 <rajnikantkori395@gmail.com>
2023-07-17 15:02:20 +0530

    some update on config

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ app.use(cors());
 app.use("/", (req, res) => {
   res.json({
     message: "Hello from express server APP",
-    routes: ` 
-    GET /sheets/:spreadsheetId/:range? - Retrieve all rows from the Google Sheet.
-    POST /sheets/:spreadsheetId/:range? - Add a new row to the Google Sheet. The request body should contain the row data in JSON format.
-    PUT /sheets/:spreadsheetId/:range? - Update a specific row in the Google Sheet based on the row ID. The request body should contain the updated row data in JSON format.
-    DELETE /sheets/:spreadsheetId/:range? - Delete a specific row from the Google Sheet based on the row ID.`,
   });
 });
 app.use("/sheets", sheetsRouter);
