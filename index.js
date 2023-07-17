7952a113881c4c58198d79f62f444c7db7c2d08a
rajnikantkori395 <rajnikantkori395@gmail.com>
2023-07-17 16:13:02 +0530

    some updates

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/", (req, res) => {
-  res.json({
-    message: "Hello from express server APP",
-  });
-});
+// app.use("/", (req, res) => {
+//   res.json({
+//     message: "Hello from express server APP",
+//   });
+// });
 app.use("/api/sheets", sheetsRouter);
+// Handle all other routes
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
