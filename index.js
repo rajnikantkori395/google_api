49fbe0372137e440790779b6cef6fb3a14538cff
rajnikantkori395 <rajnikantkori395@gmail.com>
2023-07-17 17:30:24 +0530

    minor changes on index

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ app.use(cors());
 app.use("/sheets", sheetsRouter);
 // Handle all other routes
 app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
+  res.status(404).json({
+    error: "This is a Home Route, no data here.",
+    available_routes: `GET /sheets/:spreadsheetId/:range? - Retrieve all rows from the Google Sheet.
+  POST /sheets/:spreadsheetId/:range? - Add a new row to the Google Sheet. The request body should contain the row data in JSON format.
+  PUT /sheets/:spreadsheetId/:range? - Update a specific row in the Google Sheet based on the row ID. The request body should contain the updated row data in JSON format.
+  DELETE /sheets/:spreadsheetId/:range? - Delete a specific row from the Google Sheet based on the row ID.`,
+  });
 });
 
 const PORT = process.env.PORT || 9000;
