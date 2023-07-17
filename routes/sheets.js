bfe31f0294094e20f7cf1f195b492208635c7385
rajnikantkori395 <rajnikantkori395@gmail.com>
2023-07-17 16:48:21 +0530

    routes updated

--- routes/sheets.js
+++ routes/sheets.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const sheetsController = require("../controllers/sheetsController");
+const driveController = require('../controllers/driveController');
 
 router.get("/:spreadsheetId/:range?", sheetsController.getSheet);
 router.post("/:spreadsheetId/:range?", sheetsController.createSheet);
 router.put("/:spreadsheetId/:range?", sheetsController.updateSheet);
 router.delete("/:spreadsheetId/:range?", sheetsController.deleteSheet);
+router.post('/upload', driveController.uploadFile);
 
 module.exports = router;
